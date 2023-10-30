bd66da65b9fcf5a8e557d9a69491b31bdb3a30ce
Dmitry Zemnitskiy <pragmasoft@gmail.com>
2023-10-30 02:21:55 +0200

    Initial commit

---
+++ src/test/java/ua/pragmasoft/template/MainTest.java
@@ -0,0 +1,22 @@
+package ua.pragmasoft.template;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainTest {
+  /**
+   * Logger.
+   */
+  static final Logger LOG = LoggerFactory.getLogger(MainTest.class);
+
+  /**
+   * Test template.
+   */
+  @Test
+  public void testTrue() {
+    LOG.debug("TODO replace with real test");
+    assertTrue(true);
+  }
+}
